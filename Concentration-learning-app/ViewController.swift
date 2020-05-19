78b689971144b54acde122d37388e8725217dbc7
Vasiliy Matveev <thebifid@gmail.com>
2020-05-19 18:35:35 +0700

    Lession 1

--- Concentration-learning-app/ViewController.swift
+++ Concentration-learning-app/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoises: [String] = ["🎃", "👻", "🎃", "👻"]
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoises[cardNumber], on: sender)
+        } else {
+            print("choosen card was not in cardButtons")
+        }
+        
         
     }
-
-
+    
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 }
 
